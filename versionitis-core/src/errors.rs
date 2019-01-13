6cf02bd0a9c81d2b71147753a763d79a5ecd4099
jonathan gerber <jlgerber@gmail.com>
2019-01-13 10:27:48 -0800

    add failure derivation added ParseIntError, InvalidPackageVersion, UnknownPackage

--- versionitis-core/src/errors.rs
+++ versionitis-core/src/errors.rs
@@ -1,3 +1,19 @@
-pub enum VersionitisError {
+use failure::Fail;
+use std::fmt::Debug;
 
+#[derive(Fail, Debug)]
+pub enum VersionitisError {
+    #[fail(display = "{}", _0)]
+    ParseIntError(#[fail(cause)] std::num::ParseIntError),
+    #[fail(display="InvalidPackageVersion: {}", _0)]
+    InvalidPackageVersion(String),
+    #[fail(display="UnknownPackage: {}", _0)]
+    UnknownPackage(String),
+}
+
+use std::num::ParseIntError;
+impl From<ParseIntError> for VersionitisError {
+    fn from(err: ParseIntError) -> Self {
+        VersionitisError::ParseIntError(err)
+    }
 }
