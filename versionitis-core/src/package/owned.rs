c1fa27cbfcc9a36b8ca1b93bc0fbc531a950507b
jonathan gerber <jlgerber@gmail.com>
2019-01-19 11:45:37 -0800

    lost spec and name change. weird

--- versionitis-core/src/package/owned.rs
+++ versionitis-core/src/package/owned.rs
@@ -1,61 +1,15 @@
 //pub mod interval;
 
 use crate::version_number::VersionNumber;
-use std::fmt;
+use serde_derive::{Deserialize,Serialize};
+use std::{ fmt,  /*hash::Hash*/ };
 
-use serde::{
-    de::{self, Deserializer, Visitor},
-    ser::{Serialize, SerializeStruct, Serializer},
-    Deserialize,
-};
-
-/// A named entity which is ordered, convertable to and from a
-/// string, hashable, and may of course be debuged.
-#[derive(PartialEq, PartialOrd, Eq, Ord, Hash)]
+/// Package implements Versionable trait. A VersionNumber may be comprised
+/// of one or more u16 digits
+#[derive(PartialEq, PartialOrd, Eq, Ord, Deserialize, Serialize,Hash)]
 pub struct Package {
-    name: String,
+    pub name: String,
     version: VersionNumber,
-}
-
-impl Serialize for Package {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut state = serializer.serialize_struct("Package", 1)?;
-        let value = format!("{}-{}", self.name, self.version);
-        state.serialize_field("package", &value)?;
-        state.end()
-    }
-}
-
-struct PackageVisitor;
-
-impl<'de> Visitor<'de> for PackageVisitor {
-    type Value = Package;
-
-    fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        formatter.write_str("an integer between -2^31 and 2^31")
-    }
-
-    fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
-    where
-        E: de::Error,
-    {
-        match Package::from_str(value) {
-            Ok(v) => Ok(v),
-            Err(e) => panic!("unable to deserialize: {}", e),
-        }
-    }
-}
-
-impl<'de> Deserialize<'de> for Package {
-    fn deserialize<D>(deserializer: D) -> Result<Package, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        deserializer.deserialize_str(PackageVisitor)
-    }
 }
 
 impl fmt::Debug for Package {
@@ -70,30 +24,30 @@ impl fmt::Display for Package {
     }
 }
 
+
 impl Package {
     /// Extract the package name as a &str
     pub fn name(&self) -> &str {
         self.name.as_str()
     }
 
-    /// Get the full specification for a package, in the form of "name-version",
-    /// as a String
+    /// Get the full name, in the form of "name-version", as a String
     pub fn spec(&self) -> String {
         format!("{}-{}", self.name, self.version.to_string())
     }
 
-    /// Construct a Package from a name and a VersionNumber instance.
+    /// Construct a Package from a vector of u16
     pub fn new<T: Into<String>>(name: T, version: VersionNumber) -> Self {
         Self {
             name: name.into(),
-            version,
+            version
         }
     }
 
     /// Construct a Package from a package name and three u16 values,
     /// following the semver spec.
     pub fn semver(name: &str, major: u16, minor: u16, micro: u16) -> Self {
-        let value = VersionNumber::new(vec![major, minor, micro]);
+        let value =  VersionNumber::new(vec![major, minor, micro]);
         Self::new(name, value)
     }
 
@@ -105,7 +59,7 @@ impl Package {
     }
 
     /// Not the FromString trait because of lifetime requirements
-    pub fn from_str(s: &str) -> Result<Self, crate::errors::VersionitisError> {
+    pub fn  from_str(s: &str) -> Result<Self, crate::errors::VersionitisError> {
         // todo support variants
         let pieces: Vec<&str> = s.split("-").collect();
         let mut result: Vec<u16> = Vec::new();
@@ -114,11 +68,12 @@ impl Package {
             result.push(x);
         }
 
-        Ok(Package::new(pieces[0], VersionNumber::new(result)))
+        Ok( Package::new(pieces[0], VersionNumber::new(result)))
     }
 
+
     /// Not the FromString trait because of lifetime requirements
-    pub fn from_strs(name: &str, version: &str) -> Result<Self, crate::errors::VersionitisError> {
+    pub fn  from_strs(name: &str, version: &str) -> Result<Self, crate::errors::VersionitisError> {
         // todo support variants
         let mut result: Vec<u16> = Vec::new();
         for x in version.split(".").map(|x| x.parse::<u16>()) {
@@ -126,7 +81,7 @@ impl Package {
             result.push(x);
         }
 
-        Ok(Package::new(name, VersionNumber::new(result)))
+        Ok( Package::new(name, VersionNumber::new(result)))
     }
 }
 
@@ -134,13 +89,9 @@ impl Package {
 macro_rules! version {
     ($e:expr) => {
         Package::from_str(
-            stringify!($e)
-                .chars()
-                .filter(|x| *x != ' ')
-                .collect::<String>()
-                .as_str(),
+        stringify!($e).chars().filter(|x| *x != ' ').collect::<String>().as_str()
         )
-    };
+    }
 }
 
 #[cfg(test)]
@@ -149,14 +100,14 @@ mod tests {
 
     #[test]
     fn package_implements_display_trait() {
-        let p = Package::from_str("foo-0.1.0").unwrap();
-        let pd = format!("{}", p);
+        let p  = Package::from_str("foo-0.1.0").unwrap();
+        let pd = format!("{}",p);
         assert_eq!(pd, "foo-0.1.0".to_string());
     }
 
     #[test]
     fn macrotest() {
-        let sv1 = version!(foo - 0.1.0);
+        let sv1 = version!( foo-0.1.0 ) ;
         let sv2 = Package::from_str("foo-0.1.0");
         assert_eq!(sv1, sv2);
     }
@@ -165,88 +116,88 @@ mod tests {
     fn base() {
         let package = String::from("fred-0.1.0.1");
         let sv1 = Package::from_str(&package).unwrap();
-        assert_eq!(sv1.name(), "fred");
+        assert_eq!(sv1.package(), "fred");
     }
 
     #[test]
     fn simple_new() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 1, 0);
-        let sv2 = Package::new(name.as_str(), VersionNumber::new(vec![0, 1, 0]));
+        let sv1 = Package::semver(&name, 0,1,0);
+        let sv2 = Package::new(name.as_str(), VersionNumber::new(vec![0,1,0]) );
         assert_eq!(sv1, sv2);
     }
 
     #[test]
     fn simple_equality() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 1, 0);
-        let sv2 = Package::semver(&name, 0, 1, 0);
+        let sv1 = Package::semver(&name, 0,1,0);
+        let sv2 = Package::semver(&name, 0,1,0);
         assert_eq!(sv1, sv2);
     }
 
     #[test]
     fn simple_inequality_lt() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 0, 1);
-        let sv2 = Package::semver(&name, 0, 1, 0);
+        let sv1 = Package::semver(&name, 0,0,1);
+        let sv2 = Package::semver(&name, 0,1,0);
         assert!(sv1 < sv2);
     }
 
     #[test]
     fn simple_inequality_gt() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 1, 0, 1);
-        let sv2 = Package::semver(&name, 0, 1, 0);
+        let sv1 = Package::semver(&name, 1,0,1);
+        let sv2 = Package::semver(&name, 0,1,0);
         assert!(sv1 > sv2);
     }
 
     #[test]
     fn complex_inequality_lt() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 1, 0);
-        let sv2 = Package::semver4(&name, 0, 1, 0, 0);
+        let sv1 = Package::semver(&name, 0,1,0);
+        let sv2 = Package::semver4(&name, 0,1,0,0);
         assert!(sv1 < sv2);
     }
 
     #[test]
     fn complex_inequality_lt2() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 1, 0);
-        let sv2 = Package::semver4(&name, 0, 1, 0, 1);
+        let sv1 = Package::semver(&name, 0,1,0);
+        let sv2 = Package::semver4(&name, 0,1,0,1);
         assert!(sv1 < sv2);
     }
 
     #[test]
     fn complex_inequality_gt() {
         let name = String::from("fred");
-        let sv1 = Package::semver(&name, 0, 1, 1);
-        let sv2 = Package::semver4(&name, 0, 1, 0, 1);
+        let sv1 = Package::semver(&name,0,1,1);
+        let sv2 = Package::semver4(&name, 0,1,0,1);
         assert!(sv1 > sv2);
     }
 
     #[test]
     fn version() {
         let name = String::from("fred");
-        let sv2 = Package::semver4(&name, 0, 1, 0, 1);
-        assert_eq!(sv2.to_string().as_str(), "fred-0.1.0.1");
+        let sv2 = Package::semver4(&name, 0,1,0,1);
+        assert_eq!(sv2.to_string().as_str(), "fred-0.1.0.1" );
     }
 
     #[test]
     fn to_str() {
         let name = String::from("fred");
         let package = String::from("fred-0.1.0.1");
-        let sv = Package::semver4(&name, 0, 1, 0, 1);
+        let sv = Package::semver4(&name, 0,1,0,1);
         let result = sv.to_string();
-        assert_eq!(result, package);
+        assert_eq!(result, package );
     }
 
     #[test]
     fn debug() {
         let name = String::from("fred");
         let package = String::from("fred-0.1.0.1");
-        let sv = Package::semver4(&name, 0, 1, 0, 1);
+        let sv = Package::semver4(&name, 0,1,0,1);
         let result = format!("{:?}", sv);
-        assert_eq!(result, package);
+        assert_eq!(result, package );
     }
 
     #[test]
@@ -254,7 +205,7 @@ mod tests {
         let name = String::from("fred");
         let package = String::from("fred-0.1.0.1");
         let sv1 = Package::from_str(&package).unwrap();
-        let sv2 = Package::semver4(&name, 0, 1, 0, 1);
-        assert_eq!(sv1, sv2);
+        let sv2 = Package::semver4(&name, 0,1,0,1);
+        assert_eq!(sv1, sv2 );
     }
 }
