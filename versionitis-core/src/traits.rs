680ea8660c5c4d16b7b7f2806ca0c79bc5e27ed4
jonathan gerber <jlgerber@gmail.com>
2019-01-19 10:39:52 -0800

    rustfmt

--- versionitis-core/src/traits.rs
+++ versionitis-core/src/traits.rs
@@ -2,10 +2,7 @@
 //!
 //! Define all traits for versionitis-core
 use core::str::FromStr;
-use std::{
-    fmt::Debug,
-    //error::Error,
-};
+use std::fmt::Debug;
 
 /// Trait for defining a version scheme
 pub trait Versionable: Eq + Ord + Debug + ToString + FromStr {}
@@ -14,15 +11,18 @@ pub trait Versionable: Eq + Ord + Debug + ToString + FromStr {}
 /// for tracking all of the distinct vers
 pub trait TrackPackages {
     type AddReturns;
-    type GetReturns: Debug+Eq+Ord;
+    type GetReturns: Debug + Eq + Ord;
     type Errors: failure::Fail;
 
     /// Add a package version
-    fn add_version(&mut self, package_name: &str, version: &str)
-    -> Result<Self::AddReturns, Self::Errors>;
+    fn add_version(
+        &mut self,
+        package_name: &str,
+        version: &str,
+    ) -> Result<Self::AddReturns, Self::Errors>;
 
     /// Given a package name (sans version), fetch a vector of Packages wrapped in a
     /// Result. If no package with the supplied name exits, return an UnknownPackageError,
     /// wrapped in a Result.
     fn get<'a>(&'a self, package: &str) -> Result<&'a Vec<Self::GetReturns>, Self::Errors>;
- }
+}
