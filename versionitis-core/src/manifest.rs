578896c4c490a0e9a6c5b75dcc04006476aa42c4
jonathan gerber <jlgerber@gmail.com>
2019-01-19 10:33:48 -0800

    rustfmt

--- versionitis-core/src/manifest.rs
+++ versionitis-core/src/manifest.rs
@@ -2,21 +2,17 @@
 //!
 //! stores package dependencies
 //!
-use crate::{
-    errors::VersionitisError,
-    interval::Interval,
-    package::owned::Package,
-};
-use serde::ser::{ Serialize, Serializer, SerializeStructVariant };
-use serde_derive::{Deserialize,Serialize};
+use crate::{errors::VersionitisError, interval::Interval, package::owned::Package};
+use serde::ser::{Serialize, SerializeStructVariant, Serializer};
+use serde_derive::{Deserialize, Serialize};
 use std::collections::HashSet;
 
 /// Enum wrapping possible inputs to from_src
-#[derive(Debug,PartialEq,Eq,Serialize,Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub enum PISrc<'a> {
     Single(&'a str),
     HalfOpen(&'a str, &'a str),
-    Open(&'a str, &'a str)
+    Open(&'a str, &'a str),
 }
 
 /// A package interval expresses a range of package versions
@@ -29,18 +25,19 @@ impl Serialize for PackageInterval {
         S: Serializer,
     {
         match *self {
-             Interval::Single(ref v) => {
-                serializer.serialize_newtype_variant( "Interval", 0, "single", &v.name())
+            Interval::Single(ref v) => {
+                serializer.serialize_newtype_variant("Interval", 0, "single", &v.name())
             }
 
-            Interval::HalfOpen{ref start, ref end} => {
-                let mut state = serializer.serialize_struct_variant("Interval", 0, "half_open", 2)?;
+            Interval::HalfOpen { ref start, ref end } => {
+                let mut state =
+                    serializer.serialize_struct_variant("Interval", 0, "half_open", 2)?;
                 state.serialize_field("start", &start.name())?;
                 state.serialize_field("end", &end.name())?;
                 state.end()
             }
 
-            Interval::Open{ref start, ref end} => {
+            Interval::Open { ref start, ref end } => {
                 let mut state = serializer.serialize_struct_variant("Interval", 0, "open", 2)?;
                 state.serialize_field("start", &start.name())?;
                 state.serialize_field("end", &end.name())?;
@@ -50,22 +47,15 @@ impl Serialize for PackageInterval {
     }
 }
 
-
 impl PackageInterval {
     /// Retrieve the package name for the PackageInterval as a &str.
     pub fn package_name(&self) -> &str {
-         match *self {
-            Interval::Single(ref v) => {
-                v.package()
-            }
+        match *self {
+            Interval::Single(ref v) => v.package(),
 
-            Interval::HalfOpen{ref start, ..} => {
-                start.package()
-            }
+            Interval::HalfOpen { ref start, .. } => start.package(),
 
-            Interval::Open{ref start, ..} => {
-                start.package()
-            }
+            Interval::Open { ref start, .. } => start.package(),
         }
     }
 
@@ -86,32 +76,26 @@ impl PackageInterval {
     /// ```
     pub fn from_src(input: &PISrc) -> Result<PackageInterval, VersionitisError> {
         match *input {
-            PISrc::Single(ref name) => {
-                Ok(Interval::Single(Package::from_str(name)?))
-            }
+            PISrc::Single(ref name) => Ok(Interval::Single(Package::from_str(name)?)),
 
-            PISrc::HalfOpen(ref p1, ref p2) => {
-                Ok(Interval::HalfOpen{
-                    start: Package::from_str(p1)?,
-                    end: Package::from_str(p2)?
-                })
-            }
+            PISrc::HalfOpen(ref p1, ref p2) => Ok(Interval::HalfOpen {
+                start: Package::from_str(p1)?,
+                end: Package::from_str(p2)?,
+            }),
 
-            PISrc::Open(ref p1, ref p2) => {
-                Ok(Interval::Open{
-                    start: Package::from_str(p1)?,
-                    end: Package::from_str(p2)?
-                })
-            }
+            PISrc::Open(ref p1, ref p2) => Ok(Interval::Open {
+                start: Package::from_str(p1)?,
+                end: Package::from_str(p2)?,
+            }),
         }
     }
 }
 
-pub type IntervalSet     = HashSet<PackageInterval>;
+pub type IntervalSet = HashSet<PackageInterval>;
 
 /// A manifest stores a set of dependencies for a named package.
 /// The dependencies are modeled as a HashSet<Interval<Package>>.
-#[derive(Debug,PartialEq,Eq,Serialize,Deserialize)]
+#[derive(Debug, PartialEq, Eq, Serialize, Deserialize)]
 pub struct Manifest {
     name: String,
     dependencies: IntervalSet,
@@ -125,7 +109,10 @@ impl Manifest {
     /// ```notest
     /// let manifest = Manifest::new("rustup");
     /// ```
-    pub fn new<I>(name: I) -> Self where I: Into<String> {
+    pub fn new<I>(name: I) -> Self
+    where
+        I: Into<String>,
+    {
         Self {
             name: name.into(),
             dependencies: IntervalSet::new(),
@@ -134,7 +121,7 @@ impl Manifest {
 
     /// return the name the package
     pub fn package(&self) -> &str {
-        return self.name.as_str()
+        return self.name.as_str();
     }
     /// Add a dependency to the manifest
     ///
@@ -146,10 +133,11 @@ impl Manifest {
     /// manifest.add_dependency(interval)?;
     /// ```
     pub fn add_dependency(&mut self, interval: PackageInterval) -> Result<(), VersionitisError> {
-
-        let package_name = interval.package_name();//package_name_for(&interval);
-        if  self.depends_on(package_name ) {
-            return Err(VersionitisError::DuplicatePackageDependency(package_name.to_string()));
+        let package_name = interval.package_name(); //package_name_for(&interval);
+        if self.depends_on(package_name) {
+            return Err(VersionitisError::DuplicatePackageDependency(
+                package_name.to_string(),
+            ));
         }
         self.dependencies.insert(interval);
         Ok(())
@@ -160,18 +148,12 @@ impl Manifest {
     pub fn depends_on(&self, name: &str) -> bool {
         for dep in &self.dependencies {
             let found = match dep {
-                Interval::Single(ref v) => {
-                    name == v.package()
-                }
+                Interval::Single(ref v) => name == v.package(),
                 // shouldn't need to test both start and end since
                 // the package name should be guaranteed to be the same
-                Interval::HalfOpen{ref start, ..} => {
-                    name == start.package()
-                }
+                Interval::HalfOpen { ref start, .. } => name == start.package(),
 
-                Interval::Open{ref start, ..} => {
-                    name == start.package()
-                }
+                Interval::Open { ref start, .. } => name == start.package(),
             };
             if found {
                 return true;
@@ -182,7 +164,7 @@ impl Manifest {
 
     /// Test whether a manifest has a particular versioned packaage as a
     /// dependency. For intervals, this means that the Package is contained within.
-    pub fn depends_on_package(&self, package: &Package ) -> bool {
+    pub fn depends_on_package(&self, package: &Package) -> bool {
         for dep in &self.dependencies {
             let found = dep.contains(package);
             if found {
@@ -207,7 +189,7 @@ mod tests {
 
         #[test]
         fn get_package_name() {
-            type PI=PackageInterval;
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
@@ -221,7 +203,7 @@ mod tests {
 
         #[test]
         fn add_dependencies() {
-            type PI=PackageInterval;
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
@@ -234,7 +216,7 @@ mod tests {
         #[test]
         fn add_dependencies_src() {
             // make this a bit more ergonomic to type
-            type PI=PackageInterval;
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
@@ -246,7 +228,7 @@ mod tests {
 
         #[test]
         fn cannot_add_duplicate_dependency() {
-            type PI=PackageInterval;
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
@@ -256,12 +238,15 @@ mod tests {
             manifest.add_dependency(interval1).unwrap();
             manifest.add_dependency(interval2).unwrap();
             let result = manifest.add_dependency(interval3);
-            assert!(result.is_err(), "should return err when attempting to add duplicate package");
+            assert!(
+                result.is_err(),
+                "should return err when attempting to add duplicate package"
+            );
         }
 
         #[test]
         fn depends_on() {
-            type PI=PackageInterval;
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
@@ -278,10 +263,8 @@ mod tests {
 
         #[test]
         fn depends_on_package() {
-            let pfs = |n: &str| {
-                Package::from_str(n).unwrap()
-            };
-            type PI=PackageInterval;
+            let pfs = |n: &str| Package::from_str(n).unwrap();
+            type PI = PackageInterval;
             use self::PISrc::*;
             let mut manifest = Manifest::new("fred-1.0.0");
             let interval1 = PI::from_src(&Single("foo-0.1.0")).unwrap();
